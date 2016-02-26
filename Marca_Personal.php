271bac2f34089605d052217b4e8d95e94230411f
Iker Ibisate <iker.ibis@gmail.com>
2016-02-26 21:42:37 +0100

    miau

---
+++ Marca_Personal.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Marca personal I.Ibis</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <link rel="stylesheet" type="text/css" href="aspectos.css" media="screen" />
+
+
+</head>
+
+<body>
+
+<h1>Bienvenidos a mi pequeño rinc&oacute;n</h1>
+<div style="text-align: center"><img src=""></div>
+<p>Me llamo Iker y estoy titulado en <u>administraci&oacute;n de sistemas inform&aacute;ticos</u>, actualmente estudiando <u>desarrollo de aplicaciones multiplataforma</u>.</p>
+<h2>Experiencia</h2>
+<p>He trabajado en varios lugares siempre de cara al <strong>p&uacute;blico</strong>, desde camarero a t&eacute;cnico de soporte en lugares de dominio <strong>p&uacute;blico</strong>.
+Por lo que obviamente soy una persona bastante extrovertida.
+</p>
+<h2>Aficiones</h2>
+<p>
+<ul>
+    <li>Soy un gran aficionado al &quot;Senderismo&quot; a nivel local, aunque en ocasiones tambi&eacute;n alta montaña, ya sean zonas pirenaicas o Picos de Europa por ejemplo</li>
+    <li>El llamado &quot;Running&quot;, me gusta correr al aire libre, preferiblemente fuera de ciudad</li>
+    <li>Viajar, siempre que se puede hacer una escapadita</li>
+    <li>Cine y series</li>
+    <li>Lectura, tanto libros como comics, decantandome en mayor parte por los libros</li>
+    <li>M&uacute;sica, de varios tipos</li>
+    <li>Videojuegos, en especial los clasicos</li>
+    <li>Hardware y cacharreo en general</li>
+</ul>
+</p>
+</body>
+</html>
