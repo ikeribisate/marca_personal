bd9a3da3eff77ceefed1f46d8f3e1d4827bb16d1
Iker Ibisate <iker.ibis@gmail.com>
2016-03-04 21:57:23 +0100

    miauuuu

--- index.php
+++ index.php
@@ -5,21 +5,40 @@
     <title>Marca personal I.Ibis</title>
 
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <!--<link rel="stylesheet" type="text/css" href="aspectos.css" media="screen" />-->
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="aspectos.css" media="screen" />
-
-
 </head>
 
 <body>
 
+
+
+
+<div>
 <h1>Bienvenidos a mi pequeño rinc&oacute;n</h1>
-<div style="text-align: center"><img src=""></div>
+</div>
+
+
+<div>
 <p>Me llamo Iker y estoy titulado en <u>administraci&oacute;n de sistemas inform&aacute;ticos</u>, actualmente estudiando <u>desarrollo de aplicaciones multiplataforma</u>.</p>
+</div>
+
+
 <h2>Experiencia</h2>
 <p>He trabajado en varios lugares siempre de cara al <strong>p&uacute;blico</strong>, desde camarero a t&eacute;cnico de soporte en lugares de dominio <strong>p&uacute;blico</strong>.
 Por lo que obviamente soy una persona bastante extrovertida.
 </p>
+
+
 <h2>Aficiones</h2>
+
+
 <p>
 <ul>
     <li>Soy un gran aficionado al &quot;Senderismo&quot; a nivel local, aunque en ocasiones tambi&eacute;n alta montaña, ya sean zonas pirenaicas o Picos de Europa por ejemplo</li>
@@ -32,5 +51,11 @@ Por lo que obviamente soy una persona bastante extrovertida.
     <li>Hardware y cacharreo en general</li>
 </ul>
 </p>
+
+
+<!-- jQuery first, then Bootstrap JS. -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+
 </body>
 </html>
